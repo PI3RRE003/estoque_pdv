a92dbbb2954a87a897cc46a334c3b8a2b8ec592f
PI3RRE003 <vitorcruz.dev@gmail.com>
2026-03-06 17:31:33 -0300

    ajustando front do dashboard logica de lucro produto com  desconto

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,19 +1,15 @@
 class CartController < ApplicationController
   def add
-    # --- PERSISTÊNCIA DO CLIENTE ---
-    # Se o ID do cliente vier no formulário de bip, salva na sessão
     if params[:client_id].present?
       session[:selected_client_id] = params[:client_id]
     end
 
     query = params[:barcode_or_name]
-    # Captura a quantidade (seja do botão +/- do carrinho ou do campo de entrada)
+
     qty_to_add = params[:quantity].present? ? params[:quantity].to_i : 1
 
-    # 1. Busca por ID ou Código de Barras
     product = Product.find_by(id: query) || Product.find_by(barcode: query)
 
-    # 2. Se não achou, tenta buscar por nome
     if product.nil? && query.present?
       product = Product.where("UPPER(name) LIKE ?", "%#{query.upcase}%").first
     end
@@ -24,14 +20,12 @@ class CartController < ApplicationController
       current_qty = session[:cart][id] || 0
       total_requested = current_qty + qty_to_add
 
-      # VALIDAÇÃO DE ESTOQUE
       if total_requested > product.stock_quantity
         flash[:alert] = "Estoque insuficiente! #{product.name} tem apenas #{product.stock_quantity} un."
       elsif total_requested <= 0
         session[:cart].delete(id)
       else
         session[:cart][id] = total_requested
-        # Se for um bip novo, damos o feedback. Se for alteração no +/-, não precisa de flash.
         flash[:notice] = "Adicionado: #{product.name}" if current_qty == 0
       end
     else
